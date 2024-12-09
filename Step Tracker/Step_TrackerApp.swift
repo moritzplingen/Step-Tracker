57e057a1f918f7fe46a3aeaedca6dcd2422ecbf1
Iimayankee <moritz.plingen@gmail.com>
2024-12-09 22:00:04 +0100

    Initial Commit

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Moritz Plingen on 09.12.24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
