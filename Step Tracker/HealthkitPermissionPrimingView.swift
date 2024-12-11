fd90d15b4c1124b8526fd4187c9160ab3c992db3
Iimayankee <moritz.plingen@gmail.com>
2024-12-11 13:37:24 +0100

    Added Healthkit Priming View File

---
+++ Step Tracker/HealthkitPermissionPrimingView.swift
@@ -0,0 +1,18 @@
+//
+//  HealthkitPermissionPrimingView.swift
+//  Step Tracker
+//
+//  Created by Moritz Plingen on 11.12.24.
+//
+
+import SwiftUI
+
+struct HealthkitPermissionPrimingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HealthkitPermissionPrimingView()
+}
